7bb479bdf34fad87c151a57d5d0ae3e07381409f
Patrick Connolly <patrick@myplanetdigital.com>
2013-07-31 21:35:45 -0400

    Makes sensu check reusable.

--- cookbooks-source/drupal-monitor/recipes/default.rb
+++ cookbooks-source/drupal-monitor/recipes/default.rb
@@ -1,8 +1,12 @@
 sensu_client "drupal" do
   address "192.168.33.2"
   subscriptions ["all", "drupal"]
+  additional({
+    :nagios_id => "1234567890",
+  })
 end
 
+# check_drupal requires Nagios utils.sh script
 package "nagios-plugins"
 
 remote_file "/usr/lib/nagios/plugins/check_drupal" do
@@ -13,7 +17,7 @@ remote_file "/usr/lib/nagios/plugins/check_drupal" do
 end
 
 sensu_check "check_drupal" do
-  command "PATH=$PATH:/usr/lib64/nagios/plugins:/usr/lib/nagios/plugins check_drupal -H 192.168.33.2 -U 1234567890"
+  command "PATH=$PATH:/usr/lib64/nagios/plugins:/usr/lib/nagios/plugins check_drupal -H :::address::: -U :::nagios_id:::"
   handlers ["default"]
   subscribers ["drupal"]
   interval 60
